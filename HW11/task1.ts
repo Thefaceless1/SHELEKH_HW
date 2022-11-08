d76d1353455aa76469ce131a6f3a827433719015
Thefaceless1 <zrjdif93@gmail.com>
2022-11-08 15:03:40 +0300

    HW11

---
+++ HW11/task1.ts
@@ -0,0 +1,49 @@
+/*
+#### Task 1 💻
+
+У нас есть список постов на сервере. И наша задача отрисовать текст этих постов на странице.
+
+    Но по каким то необъяснимым причинам, нам требуются посты номер 3, 7, 15, 23. Выглядит просто. Но есть нюанс 🐒
+Посты должны загружаться в определенном порядке. Сначала 15, потом 23, потом 7 и только потом 3. А если какой-то из постов не загрузиться, нам тогда необходимо  вывести в консоль ошибку.
+
+    Наша задача должна иметь универсальное решение. В любой момент может потребоваться загрузить другие посты, или больше постов или меньше.
+
+    Для получения постов использовать https://jsonplaceholder.typicode.com/posts
+    Задача может бы ть выполнена через https://jsonplaceholder.typicode.com/comments?postsId=1 либо через setTimeout.
+    ###### Реализуйте задачу двумя способами:
+- Promise chaining
+- Async / await*/
+
+import fetch from "node-fetch";
+
+type postData = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}[];
+
+// первый вариант автоматизированный
+async function getSelectedPosts (...num: number[]) {
+    const result = [];
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const post : Promise<any> = response.json().then(value  => value);
+    if (response.ok == true) {
+        for (const i of num) {
+            result.push(await post.then((value : postData) => value[i-1]));
+        }
+        return result
+    }
+    else {
+        return  new Error("Ошибка");
+    }
+}
+getSelectedPosts(15,23,7,3).then(value => console.log(value));
+
+
+
+// 2 вариант примитивный
+fetch('https://jsonplaceholder.typicode.com/posts').
+    then((value : any) => value.json(), error => console.log(error)).
+    then((value : postData)  => console.log([value[14],value[22],value[12],value[3]]));
+
---
+++ HW11/task2.ts
@@ -0,0 +1,32 @@
+/*
+#### Task 2 💻
+
++ Реализуйте приложение:
+    + Для приложения используется `https://jsonplaceholder.typicode.com/todos`
++ Напишите 2 функции `getTodos` & `getTasks`
++ Функция `getTodos` делает запрос по указанному адресу и забирает данные.
++ Функция `getTasks` принимает id пользователя и статус задачи (завершена или нет) и возвращает массив объектов c айди и названием задачи, {id:number; title: string}[] для всех завершенных или незавершенных (в зависимости от значения параметра) задач
++ Вывести полученный результат в консоль*/
+import fetch from "node-fetch";
+
+type data = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}[]
+
+function getTodos () : any {
+    return fetch(`https://jsonplaceholder.typicode.com/todos`).
+    then(value => value.json());
+}
+
+async function getTasks (userId: number , taskStatus : boolean) {
+    const getData : data = await getTodos()
+    const result: { id: number; title: string; }[] = [];
+    getData.forEach(value => {
+        if (value.userId ==  userId && taskStatus == value.completed) result.push({id:value.id,title:value.title});
+    })
+    return result;
+}
+getTasks(3,false).then(value => console.log(value));
