9019df5a0105e22b847f26e4890ca3bd6060db4c
Thefaceless1 <zrjdif93@gmail.com>
2022-12-15 10:37:56 +0300

    HW18

--- HW18/RequestData.ts
+++
@@ -1,5 +0,0 @@
-export class RequestData {
-    public static readonly basicUrl : string ="https://petstore.swagger.io/v2";
-    public static authToken : string = '';
-    public static readonly contentType : string = "application/json";
-}
---
+++ HW18/class/RequestProp.ts
@@ -0,0 +1,6 @@
+export class RequestProp {
+    public static readonly basicUrl : string = "https://rfs-lic-test-01.fors.ru";
+    public static readonly fileDir : string ="HW18/testfiles/";
+    authToken : string = '';
+}
+
